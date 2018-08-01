b3e79ad1eb3380b440a414b23cf9dede9ec865b9
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:30:40 +0300

    hello

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,6 @@
 // Put all your JavaScript in this file!
+function colorchooser() {
+	var x = prompt("enter a color:")
+	var y=document.getElementsByTagName("body")
+	y[0].style.backgroundColor = x
+}
