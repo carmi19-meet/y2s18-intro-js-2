52aae8cc61be2286de7c3b8dc3b52d98ce108a48
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 14:12:13 +0300

    javascript stuff, doms

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,10 @@
 // Create your 'me' object literal here!
+var me = {
+firstName: "Carmi",
+lastName: "Assaf-Shapira",
+favoriteFoods: ["pizza", "pasta", "ice cream", "orange juice"],
+age: 16,
+picture: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg"
+}
+var x=JSON.stringify(me);
+document.write(x);
--- separation/code.js
+++ separation/code.js
@@ -1,1 +1,2 @@
 // Add your alert() here!
+alert("my name is carmi")
