871e803b5a0bfbf0a3c499c67ad3c965e4400fe4
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 16:48:02 +0300

    stuffgit add .!

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,24 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ["./imgs/p1.jpeg", "./imgs/p2.jpeg","./imgs/p3.jpeg", "./imgs/p4.jpeg", "./imgs/p5.jpeg"];
 var currentIndex = 0;
+var x=document.getElementsByTagName("img")
+x[0].addEventListener("click", function (event) {
+	showNextPicture();
+});
+
 
 function showNextPicture() {
-  currentIndex++; // increment current picture
-  // if currentIndex is too large, start from the beginning again
+  currentIndex++;
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
-  }
+}
+	(event.target).src = pictures[currentIndex];
+
+  
+}
+
+  // increment current picture
+  // if currentIndex is too large, start from the beginning again
+
 
   // YOUR TASK: Finish this function!
-}
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -4,3 +4,19 @@ function colorchooser() {
 	var y=document.getElementsByTagName("body")
 	y[0].style.backgroundColor = x
 }
+var currentIndex = 0;
+var colors = ["red", "yellow", "green", "blue", "purple"]
+var z = document.getElementsByTagName("button")
+var t=document.getElementsByTagName("body")
+z[1].addEventListener("click", function (event) {
+	nextcolor();
+});
+function nextcolor(){
+	currentIndex++;
+  	if (currentIndex >= colors.length) {
+    	currentIndex = 0;
+    }
+	t[0].style.backgroundColor = colors[currentIndex];
+}
+
+  
